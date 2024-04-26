43d915cbe27656ad2ebc60bb6eecdfd2dbbd268b
lorenarzk <lorena@cloudflare.com>
2024-04-26 14:50:29 -0600

    first assignment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,26 @@
  */
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request) {
+    /**
+     * A map of the URLs to redirect to
+     * @param {Object} countryMap
+     */
+    const countryMap = {
+      US: "https://hello-worker.lore-reznik.workers.dev/us",
+    };
+
+    // Use the cf object to obtain the country of the request
+    // more on the cf object: https://developers.cloudflare.com/workers/runtime-apis/request#incomingrequestcfproperties
+    const country = request.cf.country;
+
+    if (country != null && country in countryMap) {
+      const url = countryMap[country];
+      // Remove this logging statement from your final output.
+      console.log(`Based on ${country}-based request, your user would go to ${url}.` )
+      return Response.redirect(url);
+    } else {
+      return fetch("https://juiceshop.lorenacf.com", request);
+    }
+  },
 };
